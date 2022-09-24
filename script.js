85d9dcc412995e839e36932edcb630fc16cf3416
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2022-09-24 17:39:05 +0200

    input scripted

--- script.js
+++ script.js
@@ -11,20 +11,40 @@
   ];
 
   const render = () => {
+    document.querySelector(".js-list").innerHTML = ``;
+
     for (const task of tasks) {
       console.log(task.content);
-      document.querySelector(
-        ".js-list"
-      ).innerHTML += `<li ${task.done ? "class=\"done\"": ""}>${task.content} <button class="js-deleteButton">🗑</button></li> `;
-      
+      document.querySelector(".js-list").innerHTML += `<li ${
+        task.done ? 'class="done"' : ""
+      }> 
+      <button>✅</button>${task.content} <button class="js-deleteButton">🗑</button></li> `;
     }
-   
-  const taskForm = document.querySelector(".js-TaskForm")  
-
-  taskForm.addEventListener("click", () =>{
-    event.preventDefault();
-  })
-
   };
-  render();
+
+  const addingNewTask = (TaskInput) => {
+    tasks.push({
+        content: TaskInput,
+      });
+      render();
+    document.querySelector(".js-TaskInput").value = "";
+  }
+
+  const taskForm = document.querySelector(".js-TaskForm");
+
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const TaskInput = document.querySelector(".js-TaskInput").value.trim();
+    if (TaskInput === "") {
+      return;
+    }
+   addingNewTask(TaskInput)
+  });
+
+  const init = () => {
+    render()
+  }
+
+  init()
 }
